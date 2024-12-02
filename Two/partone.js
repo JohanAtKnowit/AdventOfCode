aee1bf04a753057f4d59b5d80f36e392f24f6ab9
JohanAtKnowit <johan.ahsberg@knowit.se>
2024-12-02 10:58:50 +0100

    Added logic to check for steady levels.

--- Two/partone.js
+++ Two/partone.js
@@ -9,24 +9,40 @@ let numberOfUnsafeReports = 0;
 
 array2D.forEach((line, index) => {
   const allowedDifference = 3;
+  let increasing = false;
+  let decreasing = false;
   for (let i = 0; i < line.length - 1; i++) {
-    if (
-      line[i] - line[i + 1] > allowedDifference ||
-      line[i] - line[i + 1] < -allowedDifference
-    ) {
+    const difference = line[i] - line[i + 1];
+    if (difference > 0) {
+      decreasing = true;
+    } else if (difference < 0) {
+      increasing = true;
+    }
+    if (difference > allowedDifference || difference < -allowedDifference) {
       console.log(
-        "Report " +
-          (index + 1) +
-          " deemed unsafe. Safe margins exceeded at index: " +
-          (i + 1) +
-          ". Difference was: " +
-          (line[i] - line[i + 1]) +
-          "."
+        `Report ${index + 1} deemed unsafe due to exceeded safe margins.`
+      );
+      numberOfUnsafeReports++;
+      return true;
+    } else if (difference === 0) {
+      console.log(
+        `Report ${index + 1} deemed unsafe due to levels not moving.`
+      );
+      numberOfUnsafeReports++;
+      return true;
+    } else if (increasing && decreasing) {
+      console.log(
+        `Report ${index + 1} deemed unsafe due to fluxuating levels.`
       );
       numberOfUnsafeReports++;
       return true;
     }
   }
+  console.log(
+    `Report ${index + 1} deemed safe. Values were within threshhold and were ${
+      increasing ? "increasing" : decreasing ? "decreasing" : "remaining steady"
+    }.`
+  );
 });
-
-console.log("Found " + numberOfUnsafeReports + " unsafe reports.");
+const safeReports = array2D.length - numberOfUnsafeReports;
+console.log("Found " + safeReports + " safe reports.");
